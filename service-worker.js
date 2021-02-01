f1ddaeeab8bd996aeddcbf68cdc4b9f7cc2cd023
loukkyy <servan.fichet@gmail.com>
2021-02-01 23:58:14 +0100

    fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,25 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('trello-clone-store').then((cache) => cache.addAll([
-        '/css/buttons.css',
-        '/css/styles.css',
-        '/css/themes.css',
-        '/icon/app-logo.png',
-        '/dragAndDrop.js',
-        '/index.html',
-        '/script.js',
-      ])),
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
-  });
+  const staticQuizFlags = "trello-clone-store"
+  const assets = [
+    "/",
+    '/css/buttons.css',
+    '/css/styles.css',
+    '/css/themes.css',
+    '/icon/app-logo.png',
+    '/dragAndDrop.js',
+    '/index.html',
+    '/script.js',
+  ]
+self.addEventListener("install", (installEvent) => {
+    installEvent.waitUntil(
+      caches.open(staticQuizFlags).then((cache) => {
+        cache.addAll(assets)
+      })
+    )
+  })
+  self.addEventListener("fetch", (fetchEvent) => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then((res) => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
